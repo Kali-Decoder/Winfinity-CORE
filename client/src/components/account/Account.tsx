d0613ee784ab8915e3595887d332925a53e50eec
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-03-03 16:36:05 +0530

    Player Card

--- client/src/components/account/Account.tsx
+++ client/src/components/account/Account.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-
 import NextImage from '@/components/NextImage';
-
+import { addressFormatter } from '@/features/Game/lib/addressFormatter';
+import { useAccount } from 'wagmi';
 const Account = () => {
+  const {address} = useAccount();
   return (
     <>
       <div className='mt-6 flex flex-col items-center gap-3'>
         <NextImage
-          src='/images/demo-profile.png'
+           src={`https://effigy.im/a/${address}.svg`}
           alt='Image placeholder'
-          className='relative h-32 w-32 rounded-full border-4 border-primary-500'
+          className='relative h-32 w-32 rounded-full border-4 border-white'
           imgClassName='object-cover rounded-full'
           fill
         />
-        <span className='block'>IamjackRider</span>
-        <span className='text-gradient-primary block'>3950 pts.</span>
+        <span className='block'>{addressFormatter(address as `0xstring`)}</span>
+        <span className='text-gradient-primary block'>3950 XP</span>
       </div>
     </>
   );
