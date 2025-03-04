20603d7f0b24fa3996050849305726ed5d958fe5
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-03-04 22:47:14 +0530

    Adding Scripts + Deployement address

---
+++ client/src/features/Game/constants/players.tsx
@@ -0,0 +1,60 @@
+// TODO: Actually connect players online to play together
+
+export const players = [
+  {
+    address: '0xcfa038455b54714821f291814071161c9870B891',
+    image: `https://effigy.im/a/0xcfa038455b54714821f291814071161c9870B891.svg`,
+    handle: 'Jaden Chandler',
+    points: 3821,
+    country: 'United States',
+  },
+  {
+    address: '0x4A2B3c8dCFA7423E1F591C329dAa731C412EB5B1',
+    image: `https://effigy.im/a/0xcfa038455b54714821f291814071161c9870B891.svg`,
+    handle: 'Zaria Kennedy',
+    points: 2322,
+    country: 'Mexico',
+  },
+  {
+    address: '0x9fC4b8bA47B3A1e2bA4C01C5426F23d7C24E5C99',
+    image: `https://effigy.im/a/0xcfa038455b54714821f291814071161c9870B891.svg`,
+    handle: 'Sawyer Bowers',
+    points: 2281,
+    country: 'Argentina',
+  },
+  {
+    address: '0x3fD29409D03F7EeF762BDE3E8C3453C2b87A4e1D',
+    image: `https://effigy.im/a/0xcfa038455b54714821f291814071161c9870B891.svg`,
+    handle: 'Maeve Blackwell',
+    points: 1281,
+    country: 'England',
+  },
+  {
+    address: '0x7aB045E3Cf12D5d04C42A0C1F728748D1F3B9b7E',
+    image: `https://effigy.im/a/0xcfa038455b54714821f291814071161c9870B891.svg`,
+    handle: 'Marley Tyson',
+    points: 1121,
+    country: 'Canada',
+  },
+];
+
+
+export const transactions =[
+  {
+    id: 1,
+    date: '2021-09-01',
+    amount: 100,
+    type: 'Deposit/Stake',
+    status: 'Completed',
+    hash:"0x1234567890"
+  },
+  {
+    id: 2,
+    date: '2021-09-02',
+    amount: 200,
+    type: 'Claim',
+    status: 'Completed',
+    hash:"0x1234567890"
+  }
+
+]
