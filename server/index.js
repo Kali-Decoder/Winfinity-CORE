546d477acaa4e92d1cc12d3e72aa7e8db6b248ce
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-03-04 14:01:52 +0530

    update NFTreview  Dialog

--- server/index.js
+++ server/index.js
@@ -5,8 +5,13 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const { getOrCreateUser } = require("./services/index.js");
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:3000/",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.urlencoded({ extended: false }));
 require("./db/connection.js");
 
